01dd958d96d3f0a45df6073638894d1e77534eda
Jaime Fournier <jaimef@linbsd.org>
2017-05-26 14:07:51 -0400

    it works!!

--- read-ct-gerbil.scm
+++ read-ct-gerbil.scm
@@ -2,21 +2,21 @@ package: read-ct
 (export main)
 (import :std/iter :std/text/zlib :std/text/json)
 
-
 (def (main . args)
-    (for ((x (list-of-ct-files '("~/cs/"))))
-	 ;;(read-ct-file x))
-	 (displayln x)))
+  (map
+    (lambda (x)
+      (read-ct-file x))
+      (list-of-ct-files '("~/cs/"))))
 
 (def (read-ct-file file)
-  (displayln "reading:" file)
-  (map
-    (lambda (line)
-      (displayln "line:" (hash->list line)))
-    (call-with-input-file file
-      (lambda (file-input)
-	(hash->list (read-json (open-input-string (bytes->string (uncompress file-input)))))))))
-
+  ;;(displayln "reading:" file)
+  (call-with-input-file file
+    (lambda (file-input)
+      (let (( mytables (hash-ref (read-json (open-input-string (bytes->string (uncompress file-input)))) 'Records)))
+	(map
+	  (lambda (row)
+	    (displayln (hash->list (hash-ref row 'userIdentity))))
+	  mytables)))))
 
 (def (find-files file-or-dir filter)
   (if (eq? (file-type file-or-dir) 'directory)
@@ -38,6 +38,6 @@ package: read-ct
     (lambda (f)
       (find-files f
                   (lambda (filename)
-                    (and (equal? (path-extension filename) ".json.gz")
-                         (not (equal? (path-strip-directory filename) "#.json.gz"))))))
+                    (and (equal? (path-extension filename) ".gz")
+                         (not (equal? (path-strip-directory filename) "#.gz"))))))
     args)))
