b361c51823f8fcd172638e06ea38bfc7169d5bac
Jaime Fournier <jaimef@linbsd.org>
2017-05-28 02:26:37 -0400

    updates

--- read-ct-gerbil.scm
+++ read-ct-gerbil.scm
@@ -1,22 +1,72 @@
 package: read-ct
 (export main)
-(import :std/iter :std/text/zlib :std/text/json :std/srfi/19)
+
+(import
+  :gerbil/gambit/exceptions
+  :gerbil/gambit/hash
+  :gerbil/gambit/os
+  :gerbil/gambit/ports
+  :gerbil/gambit/readtables
+  :std/iter
+  :std/text/json
+  :std/text/zlib
+  :gerbil/gambit/system
+  :std/srfi/19)
 
 (def (main . args)
+  (gc-report-set! #t)
   (map
     (lambda (x)
       (read-ct-file x))
-      (list-of-ct-files '("."))))
+      (list-of-ct-files '("~/CT"))))
 
 (def (read-ct-file file)
-     (displayln "doing:" file)
      (call-with-input-file file
        (lambda (file-input)
 	 (let (( mytables (hash-ref (read-json (open-input-string (bytes->string (uncompress file-input)))) 'Records)))
 	   (map
 	    (lambda (row)
-	      (displayln (hash->list (hash-ref row 'userIdentity))))
+	      (process-row row))
+	      ;;(displayln (hash->list (hash-ref row 'userIdentity))))
 	    mytables)))))
+
+(define 101-fields (list
+		    'awsRegion
+		    'eventID
+		    'eventName
+		    'eventSource
+		    'eventTime
+		    'eventType
+		    'recipientAccountId
+		    'requestID
+		    'requestParameters
+		    'responseElements
+		    'sourceIPAddress
+		    'userAgent
+		    'userIdentity
+		    ))
+
+(define 102-fields 101-fields)
+(define 103-fields 101-fields)
+(define 104-fields 101-fields)
+(define 105-fields 101-fields)
+
+(def (process-row row)
+  (let ((ver (hash-ref row 'eventVersion)))
+    (cond
+     ((string=? ver "1.01") (parse-row 101-fields row))
+     ((string=? ver "1.02") (parse-row 102-fields row))
+     ((string=? ver "1.03") (parse-row 103-fields row))
+     ((string=? ver "1.04") (parse-row 104-fields row))
+     ((string=? ver "1.05") (parse-row 105-fields row))
+     (else (displayln "unsupported version ver:" ver)))))
+
+(def (parse-row fields row)
+  (map
+    (lambda (field)
+      (let ((val (hash-get row field)))
+	(displayln "field:" field " value:" val)))
+    fields))
 
 (def (find-files file-or-dir filter)
   (if (eq? (file-type file-or-dir) 'directory)
